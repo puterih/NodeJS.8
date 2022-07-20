9c849278bd21403b07bef512f72d95ce2181c880
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-20 19:30:30 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Validator adalah sebuah package yg mempermudah untuk melakukan validasi (verifikasi/pembuktian)
 const validator = require("validator");
+// Npm chalk(kapur) untuk mewarnai terminal/ menghias terminal sehingga menjadi warna warni.
+const chalk = require("chalk");
 
-console.log(validator.isEmail("husnulputeri@gmail.com"));
-console.log(validator.isMobilePhone("088276557813", "id-ID"));
-console.log(validator.isNumeric("1234567890"));
+// console.log(validator.isEmail("husnulputeri@gmail.com"));
+// console.log(validator.isMobilePhone("088276557813", "id-ID"));
+// console.log(validator.isNumeric("1234567890"));
+
+// console.log(chalk.black.bgBlue("Hello World!"));
+const nama = "Puteri Hsunul";
+const pesan = chalk`Lorem ipsum dolor {bgCyan.white sit amet} consectetur {bgYellow.italic.black adipisicing} elit. Nama saya : ${nama}`;
+console.log(pesan);
